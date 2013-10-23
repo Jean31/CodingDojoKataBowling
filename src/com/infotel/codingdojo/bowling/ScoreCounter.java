b759cb9c3e39fff1999e747202740131fdbfadaa
Jean PALAZUELOS <jean.palazuelos@gmail.com>
2013-10-23 08:40:04 +0200

    Variables rename

--- src/com/infotel/codingdojo/bowling/ScoreCounter.java
+++ src/com/infotel/codingdojo/bowling/ScoreCounter.java
@@ -8,30 +8,30 @@ public class ScoreCounter {
 
 	}
 
-	public Integer getScore(final List<String> partie) 
+	public Integer getScore(final List<String> game) 
 	{
 		Integer result = new Integer(0);
 
-		Iterator<String> iter = partie.iterator();  //___\ Changer la boucle de controle pour eviter les index plus bas ...
-		while (iter.hasNext())						//   /
+		Iterator<String> iter = game.iterator();  //___ Changer la boucle de controle pour eviter les index plus bas ...
+		while (iter.hasNext())					  //   
 		{
-			Integer trie1 = Integer.parseInt(iter.next());
+			Integer roll1 = Integer.parseInt(iter.next());
 
-			Integer trie2 = Integer.parseInt(iter.next());
+			Integer roll2 = Integer.parseInt(iter.next());
 
-			if (isSpare(trie1, trie2)) 
+			if (isSpare(roll1, roll2)) 
 			{
 				// Beurk
-				Integer trie3 = Integer.parseInt(partie.get(partie.indexOf(trie2)+1)); // Index intermediaire
-				result = trie1 + trie2 + trie3;
+				Integer roll3 = Integer.parseInt(game.get(game.indexOf(roll2)+1)); // Index intermediaire
+				result = roll1 + roll2 + roll3;
 			} else {
-				result += trie1 + trie2;
+				result += roll1 + roll2;
 			}
 		}
 		return result;
 	}
 
-	private boolean isSpare(Integer trie1, Integer trie2) {
-		return (trie1 + trie2) == 10;
+	private boolean isSpare(Integer roll1, Integer roll2) {
+		return (roll1 + roll2) == 10;
 	}
 }
