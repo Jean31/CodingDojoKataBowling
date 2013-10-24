1916c09936e07ca9548f7e6ad3dccc4badebca0f
Jean PALAZUELOS <jean.palazuelos@gmail.com>
2013-10-24 11:15:20 +0200

    Renommage de Variables Bonn il reste le mot value qui apparait qui n'est pas bon, mais il faut differentier la valeur string du Integer ...

--- src/com/infotel/codingdojo/bowling/ScoreCounter.java
+++ src/com/infotel/codingdojo/bowling/ScoreCounter.java
@@ -10,38 +10,38 @@ public class ScoreCounter {
 
 	public Integer getScore(final List<String> game) 
 	{
-		Integer result = new Integer(0);
+		Integer score = new Integer(0);
 
 		Iterator<String> iter = game.iterator();  
 		while (iter.hasNext())					     
 		{
-			String currentRoll = iter.next();
-			Integer roll1 = Integer.parseInt(currentRoll);
+			String firstRoll = iter.next();
+			Integer firstValueRoll = Integer.parseInt(firstRoll);
 			
-			if (isStrike(roll1)) {
-				Integer roll3 = Integer.parseInt(game.get(game.indexOf(currentRoll)+1));
-				Integer roll4 = Integer.parseInt(game.get(game.indexOf(currentRoll)+2));
-				result += roll1+roll3+roll4;
+			if (isStrike(firstValueRoll)) {
+				Integer firstNextValueRoll = Integer.parseInt(game.get(game.indexOf(firstRoll)+1));
+				Integer secondNexValuetRoll = Integer.parseInt(game.get(game.indexOf(firstRoll)+2));
+				score += firstValueRoll + firstNextValueRoll + secondNexValuetRoll;
 			} else {
-				String currentRoll2 = iter.next();
-				Integer roll2 = Integer.parseInt(currentRoll2);
+				String secondRoll = iter.next();
+				Integer secondValueRoll = Integer.parseInt(secondRoll);
 				
-				if (isSpare(roll1, roll2)) 
+				if (isSpare(firstValueRoll, secondValueRoll)) 
 				{
-					Integer roll3 = Integer.parseInt(game.get(game.indexOf(currentRoll2)+1));
-					result += roll1 + roll2 + roll3;
+					Integer nextValueRoll = Integer.parseInt(game.get(game.indexOf(secondRoll)+1));
+					score += firstValueRoll + secondValueRoll + nextValueRoll;
 					
 				} else {
-					result += roll1 + roll2;
+					score += firstValueRoll + secondValueRoll;
 				}
 			} 
 				
 		}
-		return result;
+		return score;
 	}
 
-	private boolean isSpare(Integer roll1, Integer roll2) {
-		return (roll1 + roll2) == 10;
+	private boolean isSpare(Integer firstRoll, Integer secondRoll) {
+		return (firstRoll + secondRoll) == 10;
 	}
 	
 	private boolean isStrike(Integer roll) {
